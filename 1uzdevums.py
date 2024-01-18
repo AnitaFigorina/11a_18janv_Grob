ec71f20c81f5923c033ba6f2d83af91d4de266a1
AnitaFigorina <anita.figorina@grobinasskola.lv>
2024-01-18 14:26:22 +0200

    1.uzdevums

--- 1uzdevums.py
+++ 1uzdevums.py
@@ -5,12 +5,9 @@ def lasit_un_drukaz(teksta_fails):
             print("Faila saturs:")
             print(saturs)
     except FileNotFoundError:
-        print(f"Fails ar nosaukumu '{teksta_fails}' netika atrasts.")
+        print(f"Fails ar nosaukumu '{teksta_fails}' NAV ticis atrasts.")
     except Exception as e:
         print(f"Radās kļūda: {e}")
 
-# Mainīgais ar faila nosaukumu, piemēram, 'mans_fails.txt'
 mans_fails = 'teksts.txt'
-
-# Izsauc funkciju ar norādīto faila nosaukumu
 lasit_un_drukaz(mans_fails)
